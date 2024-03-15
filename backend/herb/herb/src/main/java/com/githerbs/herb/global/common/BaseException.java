546f7c710326d2de7034d922260a6b44b2797c8c
남혜미 <nhm0525@naver.com>
2024-03-15 16:06:02 +0900

    feat-backend : 기본 설정 추가 (BaseException, BaseResponse ...)

---
+++ backend/herb/herb/src/main/java/com/githerbs/herb/global/common/BaseException.java
@@ -0,0 +1,20 @@
+package com.githerbs.herb.global.common;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class BaseException extends RuntimeException {
+	private final ErrorCode errorCode;
+
+	public int getStatus() {
+		return errorCode.getStatus();
+	}
+
+	public String getCode() {
+		return errorCode.getCode();
+	}
+}

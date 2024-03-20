9c813b3e14664dc4a933a515db69d55865ac9729
이종재 <ljj452@naver.com>
2024-03-20 17:47:40 +0900

    fix : 멤버 Repository 어노테이션 누락 수정

--- backend/githerbs/src/main/java/com/happiness/githerbs/domain/member/repository/MemberRepository.java
+++ backend/githerbs/src/main/java/com/happiness/githerbs/domain/member/repository/MemberRepository.java
@@ -1,8 +1,10 @@
 package com.happiness.githerbs.domain.member.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.happiness.githerbs.domain.member.entity.Member;
 
+@Repository
 public interface MemberRepository extends JpaRepository<Member, Integer> {
 }

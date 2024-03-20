8a1c05ae0849836b454da0447681b894517a0e69
dig04214 <dig04214@naver.com>
2024-03-20 12:31:22 +0900

    Chore : jwt, swagger 추가

--- backend/githerbs/src/main/java/com/happiness/githerbs/GitherbsApplication.java
+++ backend/githerbs/src/main/java/com/happiness/githerbs/GitherbsApplication.java
@@ -2,8 +2,10 @@ package com.happiness.githerbs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class GitherbsApplication {
 
 	public static void main(String[] args) {

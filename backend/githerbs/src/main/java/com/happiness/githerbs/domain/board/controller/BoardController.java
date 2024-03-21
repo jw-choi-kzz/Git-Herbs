2697537373b72f116d6519f7a306d3925c76bc09
이종재 <ljj452@naver.com>
2024-03-21 17:27:49 +0900

    feat-backend : ResponseEntity 수정 및 토큰 필요 api 추가

--- backend/githerbs/src/main/java/com/happiness/githerbs/domain/board/controller/BoardController.java
+++ backend/githerbs/src/main/java/com/happiness/githerbs/domain/board/controller/BoardController.java
@@ -1,5 +1,6 @@
 package com.happiness.githerbs.domain.board.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,14 +8,16 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.happiness.githerbs.domain.auth.service.JwtService;
 import com.happiness.githerbs.domain.board.dto.request.BoardRequestDto;
 import com.happiness.githerbs.domain.board.service.BoardService;
 import com.happiness.githerbs.domain.board.service.FavoriteService;
+import com.happiness.githerbs.global.common.response.SuccessResponse;
 
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -26,49 +29,56 @@ public class BoardController {
 
 	private final BoardService boardService;
 	private final FavoriteService favoriteService;
-
+	private final JwtService token;
 
 	// 자랑하기 글 작성
 	@PostMapping
-	public ResponseEntity<Object> writeBoard(@RequestBody BoardRequestDto request) {
-		Integer memberId = 1;
-		return ResponseEntity.ok().body(boardService.writeBoard(memberId, request));
+	public ResponseEntity<SuccessResponse<?>> writeBoard(@RequestHeader String authorization,@RequestBody BoardRequestDto request) {
+		Integer memberId = token.validateToken(authorization).getMemberId();
+		return ResponseEntity.ok(
+			new SuccessResponse<>(HttpStatus.OK.value() , boardService.writeBoard(memberId,request)));
+
 	}
 
 	// 자랑하기 글 전체 조회
 	@GetMapping
-	public ResponseEntity<Object> getAllBoard() {
-		Integer memberId = 1;
-		return ResponseEntity.ok().body(boardService.getAll(memberId));
-		// return ResponseEntity.ok().body("test");
+	public ResponseEntity<SuccessResponse<?>> getAllBoard(@RequestHeader(required = false) String authorization) {
+		int memberId = 0 ;
+		if(authorization != null) memberId = token.validateToken(authorization).getMemberId();
+		return ResponseEntity.ok(
+			new SuccessResponse<>(HttpStatus.OK.value() , boardService.getAll(memberId)));
 	}
 
 	//내가 올린 자랑하기 게시글 삭제
 	@DeleteMapping("/{boardId}")
-	public ResponseEntity<Object> deleteBoard(@PathVariable Integer boardId) {
-		Integer memberId = 1;
+	public ResponseEntity<SuccessResponse<?>> deleteBoard(@RequestHeader String authorization ,@PathVariable Integer boardId) {
+		Integer memberId = token.validateToken(authorization).getMemberId();
+		System.out.println(memberId);
 		boardService.removeBoard(memberId, boardId);
-		return ResponseEntity.ok().body("삭제 처리가 완료되었습니다.");
+		return ResponseEntity.ok(
+			new SuccessResponse<>(HttpStatus.OK.value(), "삭제가 완료되었습니다."));
 	}
 
 	// 내가 올린 게시글 확인
 	@GetMapping("/my-board")
-	public ResponseEntity<Object> myBoard() {
-		Integer memberId = 1 ;
-		return ResponseEntity.ok().body(boardService.getMyBoard(memberId));
+	public ResponseEntity<SuccessResponse<?>> myBoard(@RequestHeader String authorization) {
+		Integer memberId = token.validateToken(authorization).getMemberId();
+		return ResponseEntity.ok(new SuccessResponse<>(HttpStatus.OK.value(), boardService.getMyBoard(memberId)));
 	}
 
 	// 내가 좋아요 누른 게시글 확인
 	@GetMapping("/my-like")
-	public ResponseEntity<Object> myFavorite() {
-		Integer memberId = 1;
-		return ResponseEntity.ok().body(boardService.myFavorite(memberId) );
+	public ResponseEntity<SuccessResponse<?>> myFavorite(@RequestHeader String authorization) {
+		Integer memberId = token.validateToken(authorization).getMemberId();
+		return ResponseEntity.ok(
+			new SuccessResponse<>(HttpStatus.OK.value(), boardService.myFavorite(memberId)));
 	}
 
 	//좋아요 누르기
 	@PutMapping("/{boardId}")
-	public ResponseEntity<Object> favoriteWrite(@PathVariable int boardId) {
-		Integer memberId = 1;
-		return ResponseEntity.ok().body(favoriteService.saveFavorite(memberId,boardId));
+	public ResponseEntity<SuccessResponse<?>> favoriteWrite(@RequestHeader String authorization, @PathVariable int boardId) {
+		Integer memberId = token.validateToken(authorization).getMemberId();
+		return ResponseEntity.ok(
+			new SuccessResponse<>(HttpStatus.OK.value(), favoriteService.saveFavorite(memberId,boardId)));
 	}
 }

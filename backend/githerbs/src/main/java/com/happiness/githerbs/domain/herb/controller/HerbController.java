f23f61935eda9e31a084ad10cf75a7f7f700cdbf
남혜미 <nhm0525@naver.com>
2024-03-25 08:42:40 +0900

    fix-backend : 도감 조회 정렬 수정

--- backend/githerbs/src/main/java/com/happiness/githerbs/domain/herb/controller/HerbController.java
+++ backend/githerbs/src/main/java/com/happiness/githerbs/domain/herb/controller/HerbController.java
@@ -3,14 +3,17 @@ package com.happiness.githerbs.domain.herb.controller;
 import java.util.List;
 
 import org.apache.tomcat.util.http.parser.Authorization;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.happiness.githerbs.domain.auth.service.JwtService;
@@ -32,11 +35,20 @@ public class HerbController {
 
 	@GetMapping
 	public ResponseEntity<SuccessResponse<Slice<HerbResponseDto>>> getHerbList(
-		@RequestHeader String authorization, Pageable pageable) {
+		@RequestHeader String authorization,
+		@RequestParam(required = false, defaultValue = "0") int page,
+		@RequestParam(required = false, defaultValue = "100")  int size, String criteria) {
 		int userId = 0;
-		if(authorization != null){
+		if (authorization != null) {
 			userId = jwtService.validateToken(authorization).getMemberId();
 		}
+
+		Pageable pageable;
+		if (criteria != null) {
+			pageable = PageRequest.of(page, size, Sort.by(Sort.Direction.DESC, criteria));
+		} else
+			pageable = PageRequest.of(page, size);
+
 		return ResponseEntity.ok(
 			new SuccessResponse<>(HttpStatus.OK.value(), herbService.getHerListByUserId(userId, pageable)));
 	}

f86d3b56a72f35c977d2b70f4470ec9664030284
강태연 <kangty1028@naver.com>
2024-03-27 08:42:45 +0900

    fix: 약초 검색 API 수정

--- backend/githerbs/src/main/java/com/happiness/githerbs/domain/search/service/SearchServiceImpl.java
+++ backend/githerbs/src/main/java/com/happiness/githerbs/domain/search/service/SearchServiceImpl.java
@@ -3,7 +3,14 @@ package com.happiness.githerbs.domain.search.service;
 import java.time.LocalDate;
 import java.util.List;
 
+import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
 import org.springframework.data.elasticsearch.core.SearchHits;
+import org.springframework.data.elasticsearch.core.query.Criteria;
+import org.springframework.data.elasticsearch.core.query.CriteriaQuery;
+import org.springframework.data.elasticsearch.core.query.HighlightQuery;
+import org.springframework.data.elasticsearch.core.query.highlight.Highlight;
+import org.springframework.data.elasticsearch.core.query.highlight.HighlightField;
+import org.springframework.data.elasticsearch.core.query.highlight.HighlightFieldParameters;
 import org.springframework.stereotype.Service;
 
 import com.happiness.githerbs.domain.herb.entity.Herb;
@@ -13,26 +20,26 @@ import com.happiness.githerbs.domain.search.dto.response.SearchResponseDto;
 import com.happiness.githerbs.domain.search.entity.HerbDocument;
 import com.happiness.githerbs.domain.search.entity.SearchLog;
 import com.happiness.githerbs.domain.search.repository.SearchLogRepository;
-import com.happiness.githerbs.domain.search.repository.SearchRepository;
 import com.happiness.githerbs.global.common.code.ErrorCode;
 import com.happiness.githerbs.global.common.exception.BaseException;
 
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class SearchServiceImpl implements SearchService {
 
-	private final SearchRepository searchRepository;
 	private final HerbRepository herbRepository;
 	private final MemberRepository memberRepository;
 	private final SearchLogRepository searchLogRepository;
+	private final ElasticsearchOperations elasticsearchOperations;
 
 	@Override
 	@Transactional
 	public List<SearchResponseDto> search(Integer memberId, String keyword) {
-		SearchHits<HerbDocument> searchHits = searchRepository.searchByKeyword(keyword);
 		if (memberId != null) {
 			SearchLog searchLog = SearchLog.builder()
 				.member(
@@ -42,13 +49,42 @@ public class SearchServiceImpl implements SearchService {
 				.build();
 			searchLogRepository.save(searchLog);
 		}
+		String herbMedicinalEffect = "herbMedicinalEffect";
+		Criteria criteria = new Criteria()
+			.subCriteria(
+				(new Criteria("herbName").matches(keyword).boost(1.0f))
+					.or(new Criteria("herbNickname").matches(keyword).boost(1.0f))
+					.or(new Criteria("herbScientificName").matches(keyword).boost(1.0f))
+					.or(new Criteria("herbHarvestingTime").matches(keyword).boost(1.0f))
+					.or(new Criteria("herbEnvironment").matches(keyword).boost(1.0f))
+					.or(new Criteria("herbQuality").matches(keyword).boost(1.0f))
+					.or(new Criteria("herbMedicalPart").matches(keyword).boost(1.0f))
+					.or(new Criteria(herbMedicinalEffect).matches(keyword).boost(1.0f)));
+
+		HighlightFieldParameters highlightFieldParameters = HighlightFieldParameters.builder()
+			.withPreTags("<br>")
+			.withPostTags("</br>")
+			.withFragmentSize(50)
+			.build();
+
+		HighlightField highlightField = new HighlightField(herbMedicinalEffect, highlightFieldParameters);
+
+		Highlight highlight = new Highlight(List.of(highlightField));
+
+		HighlightQuery highlightQuery = new HighlightQuery(highlight, String.class);
+
+		CriteriaQuery searchQuery = new CriteriaQuery(criteria);
+		searchQuery.setHighlightQuery(highlightQuery);
+
+		SearchHits<HerbDocument> searchHits = elasticsearchOperations.search(searchQuery, HerbDocument.class);
+
 		return searchHits.stream().map(hit -> {
 			Integer herbId = hit.getContent().getId();
 			Herb herb = herbRepository.findById(herbId).orElseThrow(() -> new BaseException(ErrorCode.HERB_NOT_FOUND));
-			String description = hit.getHighlightField("herbMedicinalEffect")
+			String description = hit.getHighlightField(herbMedicinalEffect)
 				.stream()
 				.findFirst()
-				.orElseGet(() -> hit.getContent().getHerbMedicinalEffect().get(0));
+				.orElseGet(() -> hit.getContent().getHerbMedicinalEffect().get(0).substring(0, 50));
 			return SearchResponseDto.builder()
 				.id(herbId)
 				.herbName(herb.getHerbName())

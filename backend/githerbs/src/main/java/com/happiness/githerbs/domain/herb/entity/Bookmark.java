f7aa3f18e915e7642b2a47ac13a264f43abb4701
남혜미 <nhm0525@naver.com>
2024-03-28 11:26:07 +0900

    fix-backend : bookmark entity 수정 (createdAt 값 자동 생성)

--- backend/githerbs/src/main/java/com/happiness/githerbs/domain/herb/entity/Bookmark.java
+++ backend/githerbs/src/main/java/com/happiness/githerbs/domain/herb/entity/Bookmark.java
@@ -3,11 +3,13 @@ package com.happiness.githerbs.domain.herb.entity;
 import java.time.LocalDateTime;
 
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import com.happiness.githerbs.domain.member.entity.Member;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -26,6 +28,7 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
 @Table(name = "bookmark")
 public class Bookmark {
 

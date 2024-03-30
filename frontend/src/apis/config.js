5f6eb093e910fd616f6536fab46db885c795fe31
이종재 <149658209+JongJae2@users.noreply.github.com>
2024-03-31 04:08:52 +0900

    fix-frontend : 헤더 config 토큰 부분 수정

--- frontend/src/apis/config.js
+++ frontend/src/apis/config.js
@@ -8,7 +8,7 @@ export const configService = {
      */
     loginConfig: () => ({
         headers: {
-          Authorization: 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhY2Nlc3MtdG9rZW4iLCJleHAiOjE3MTE3NzI3ODcsImlkIjoxLCJuaWNrbmFtZSI6InRlc3QiLCJzY29wZSI6Ik1FTUJFUiJ9.mpOzOQJMfbGdx8g4QjT-bOVl42sc4QH6HVhyscGQ3RZIXEqncyyA0zS-8gXVDbkjPaY_TYIhHBLOWwfniNcnqg'
+          // Authorization: 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhY2Nlc3MtdG9rZW4iLCJleHAiOjE3MTE3NzI3ODcsImlkIjoxLCJuaWNrbmFtZSI6InRlc3QiLCJzY29wZSI6Ik1FTUJFUiJ9.mpOzOQJMfbGdx8g4QjT-bOVl42sc4QH6HVhyscGQ3RZIXEqncyyA0zS-8gXVDbkjPaY_TYIhHBLOWwfniNcnqg'
         }
       }),
     

8cd98432a546dad05d6ef93e19fdceec7ebf1470
이종재 <ljj452@naver.com>
2024-04-02 14:03:35 +0900

    feat-frontend : 토큰값 기반 api

--- frontend/src/apis/config.js
+++ frontend/src/apis/config.js
@@ -4,11 +4,24 @@ export const configService = {
     /**
      * 로그인이 필요할 경우 넣는 cofnig
      */
-    loginConfig: () => ({
-        headers: {
-          Authorization: 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhY2Nlc3MtdG9rZW4iLCJleHAiOjE3MTIxMDI5NTEsImlkIjoxLCJuaWNrbmFtZSI6InRlc3QiLCJzY29wZSI6Ik1FTUJFUiJ9.467WpujJlNoIC_oEeJBH9FZ6gITDoHKRuCZLkOyY6sAsrQseYIV4xI5frKHb3mLDFEMjpd2rDBzTzVBG1Ryifg'
-        }
-      }),
+    loginConfig: () => {
+      // 로컬스토리지에서 token 값을 가져옴
+      const token = localStorage.getItem('accessToken');
+  
+      // token 값이 존재하는지 확인
+      if (token) {
+        // token이 존재하면 Bearer 토큰을 생성하여 Authorization 헤더에 추가
+        return {
+          headers: {
+            Authorization: `Bearer ${token}`
+          }
+        };
+      } else {
+        // token 값이 없으면 빈 객체 반환
+        return {};
+      }
+    },
+  
     
 
 

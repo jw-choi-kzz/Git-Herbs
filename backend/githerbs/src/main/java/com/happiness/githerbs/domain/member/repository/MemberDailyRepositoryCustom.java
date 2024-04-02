5d4eb0290bf38ae55cdbdebdad7e653de37d122e
남혜미 <nhm0525@naver.com>
2024-04-02 13:43:54 +0900

    feature-backend : 잔디 조회 기능 구현

--- backend/githerbs/src/main/java/com/happiness/githerbs/domain/member/repository/MemberDailyRepositoryCustom.java
+++ backend/githerbs/src/main/java/com/happiness/githerbs/domain/member/repository/MemberDailyRepositoryCustom.java
@@ -4,11 +4,12 @@ import java.time.LocalDate;
 import java.util.List;
 
 import com.happiness.githerbs.domain.event.dto.response.RankingResponse;
+import com.happiness.githerbs.domain.member.dto.common.GrassDto;
 
 public interface MemberDailyRepositoryCustom {
 
+	List<GrassDto> findGrass(Integer id);
 	List<RankingResponse> findRanker(LocalDate start, LocalDate now);
-
 	void updateDailyQuiz(Integer userId, boolean correct);
 	void updateDailyBookmark(Integer userId);
 }

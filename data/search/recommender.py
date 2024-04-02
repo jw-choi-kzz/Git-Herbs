fc114dec8a7b89b9f7f176c1d8c563b4d7931d81
남혜미 <nhm0525@naver.com>
2024-04-02 17:18:51 +0900

    fix-data : 불용어 처리

--- data/search/recommender.py
+++ data/search/recommender.py
@@ -1,14 +1,28 @@
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
+from konlpy.tag import Okt
 
 file = "./herb_medicinal_effect.csv"
 herb_data = pd.read_csv(file, encoding="CP949")
 
+t = Okt()
 
 # 데이터 전처리
 # herb_effect_data = herb_effect_data[['herb_id','medicinal_effect']]
 herb_data['medicinal_effect'] = herb_data['medicinal_effect'].fillna('') # 결측값변경
+
+stop_words = ['는', '은', '이', '가', '하', '아', '것', '들', '의', '있', '되', '수', '보', '주', '등', '한', '에', '를', '과', '만', 'же', '으로', '또', '여', '게', '때', '래', '구', '면', '좀', '므로', '그', '다', '저', '여', '렇', '에서', '면', '있', '졌', '며', '거', '란', '듯', '까지', '바로', '안', '아니', '없', '위해', '밖에', '되', '곳']
+
+medicinal_effect = []
+for row in herb_data['medicinal_effect'] :
+    content_token = t.morphs(row)
+    sentence = ""
+    for word in content_token : 
+        if not word in stop_words : 
+            sentence = sentence + " " + word
+    medicinal_effect.append(sentence)
+herb_data['medicinal_effect'] = medicinal_effect
 
 # TF-IDF 
 tfidf = TfidfVectorizer()
